f772e59b52bd10a75c9e898288ddba728748ef13
arnold-wiznet <arnoldho@wiznet.hk>
2025-11-25 10:26:12 +0800

    Change FOrmat

--- TestModule/RenamedPython.py
+++ TestModule/RenamedPython.py
@@ -15,8 +15,8 @@ def TestAdd(a :int, b:int):
     :param b: Description
     :type b: int
     
-    :returns c: Added Value
-    :type c: int
+    :returns: Added Value
+    :rtype: int
     """
     c = a + b
     return c
@@ -31,8 +31,8 @@ def TestSubtract(a:int, b:int):
     :param b: Description
     :type b: int
 
-    :returns c: Subtracted Value
-    :type c: int
+    :returns: Subtracted Value
+    :rtype: int
     """
     c = a - b
     return c
