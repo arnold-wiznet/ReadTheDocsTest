912bb35b3aaf5e0dc6a6a716adcd1ae8eda2ea0b
arnold-wiznet <arnoldho@wiznet.hk>
2025-11-25 09:06:59 +0800

    add function

---
+++ TestModule/Hello.py
@@ -0,0 +1,19 @@
+def TestFunciton():
+
+    """
+    Test auto update.
+    
+    """
+    print('Hello World!')
+
+def TestAdd(a :int, b:int):
+    """
+    Docstring for TestAdd
+    
+    :param a: Description
+    :type a: int
+    :param b: Description
+    :type b: int
+    
+    """
+    return a+b
